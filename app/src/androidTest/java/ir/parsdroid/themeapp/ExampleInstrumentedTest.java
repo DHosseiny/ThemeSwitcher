480dba0247d29b96ea3f4b601877bee97932bd00
Seyyed davood hosseini <hosseinydavid@gmail.com>
2020-03-22 19:25:49 +0430

    Fix broken unit test

--- app/src/androidTest/java/ir/parsdroid/themeapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ir/parsdroid/themeapp/ExampleInstrumentedTest.java
@@ -20,6 +20,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("ir.parsdroidgroup.themeapp", appContext.getPackageName());
+        assertEquals("ir.parsdroid.themeapp", appContext.getPackageName());
     }
 }
